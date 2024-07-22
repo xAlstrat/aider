531a68960bdfdfff88caede1cfb75f751b86a7db
Ednar Echeverria <ednarecheverria.neuralworks@latam.com>
2024-07-21 23:07:12 -0400

    Implemented base format class for edit agents

---
+++ aider/agents/edit_formats/base_format.py
@@ -0,0 +1,16 @@
+class BaseDiffFormat:
+
+    diff_format_instructions = None
+    system_reminder = None
+    agent = None
+    
+    def get_edits(self):
+        # raise not implemented
+        raise NotImplementedError()
+
+    def apply_edits(self, edits):
+        # raise not implemented
+        raise NotImplementedError()
+    
+    def format_file_diff(self, file_path, file_language, original_full=None, updated_full=None, chunks=None):
+        raise NotImplementedError()
